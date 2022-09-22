ce609840422d5a05997dac43cd57982cc55234c0
hitesh saini <hiteshsaini639@gmail.com>
2022-09-22 09:04:14 +0530

    Added home,about and cart page

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const nav = document.querySelector(".nav");
 const close = document.getElementById("close");
 const popupImage = document.getElementById("popup-image");
-window.addEventListener("scroll", fixNav);
 const cardContainer = document.querySelector(".card-container");
 const popupContainer = document.getElementById("popup-container");
+const notification = document.getElementById("notification");
+const cartItems = document.getElementById("cart-items");
+const cart = document.getElementById("cart");
+const cartBtn = document.querySelector(".cart-btn");
+
+cartBtn.addEventListener("click", () => {
+  cart.classList.toggle("active-cart");
+});
+
+window.addEventListener("scroll", fixNav);
 function fixNav() {
   if (window.scrollY > nav.offsetHeight + 150) {
     nav.classList.add("active");
@@ -11,6 +20,15 @@ function fixNav() {
     nav.classList.remove("active");
   }
 }
+
+cardContainer.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    const imgElement =
+      e.target.parentElement.previousElementSibling.firstElementChild;
+    createNotification(imgElement.alt);
+    addToCart(imgElement);
+  }
+});
 
 cardContainer.addEventListener("dblclick", (e) => {
   if (e.target.matches("img")) {
@@ -23,3 +41,31 @@ close.addEventListener("click", () => {
   popupContainer.classList.remove("popup-active");
   popupImage.src = "";
 });
+
+function createNotification(notificationMsg) {
+  const div = document.createElement("div");
+  div.classList.add("show");
+  div.innerText = `Image:${notificationMsg} is added to the cart`;
+  notification.appendChild(div);
+  (() => {
+    setTimeout(() => {
+      div.remove();
+    }, 3000);
+  })();
+}
+
+function addToCart(item) {
+  const newItem = `<div class="cart-row">
+  <span class="cart-item cart-column">
+    <img class="cart-img" src="${item.src}" alt="" />
+    <span>${item.alt}</span>
+  </span>
+  <span class="cart-price cart-column">${item.dataset.price}</span>
+  <span class="cart-quantity cart-column">
+    <input type="number" value="1" />
+    <button>REMOVE</button>
+  </span>
+</div>`;
+  console.log(cartItems);
+  cartItems.innerHTML += newItem;
+}
